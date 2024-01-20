72e4b119342682b4e493638fa84d0a13fe026021
azapru <karolgrabinski2018@gmail.com>
2024-01-20 21:37:35 +0100

    fix mah silly mistake

--- home/script.js
+++ home/script.js
@@ -2,5 +2,4 @@ function randomInteger(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-console.log(randomInteger(1, 10))
-document.getElementById("sticker").src = "stickers/"+randomInteger(1, 12)+".webp"
+document.getElementById("sticker").src = "stickers/"+randomInteger(1, 7)+".webp"
