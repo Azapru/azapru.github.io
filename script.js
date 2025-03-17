461d3545f6cfb9c54d264ad4ce2abc2a17c6736d
azapru <karolgrabinski2018@gmail.com>
2025-03-17 13:55:43 +0100

    rewritten how azzierium desktop apps work behind the scenes

--- script.js
+++ script.js
@@ -9,86 +9,93 @@ let desktopDragPosY = 0
 let desktopDrag = false
 
 // Function for creating windows for apps
-function createWindow(app, title, x, y, width, height, allowResize, center) {
-    const appWindow = document.createElement('div');
-    appWindow.className = "appWindow";
-    appWindow.style.zIndex = zIndexCounter++; // Make sure it on top
-    document.getElementById("watermark").style.zIndex = zIndexCounter++; // Make sure the watermark always stays on top
-    if (center == true) {
-        appWindow.style.left = `${window.innerWidth/2 - width/2}px`;
-        appWindow.style.top = `${window.innerHeight/2 - height/2}px`;
-    } else {
-        let posX = x/100 * window.innerWidth
-        let posY = y/100 * window.innerHeight
+function createWindow(app) {
+    fetch(`apps/${app}/app.json`)
+        .then(response => response.json())
+        .then(data => {
+            title = data["window_title"]
+            x = data["x"]
+            y = data["y"]
+            width = data["width"]
+            height = data["height"]
+            allowResize = data["allow_resizing"]
+            center = data["center"]
 
-        if (posX > (window.innerWidth / 2)) {
-            posX = posX - width
-        }
-        if (posY > (window.innerHeight / 2)) {
-            posY = posY - height
-        }
+            const appWindow = document.createElement('div');
+            appWindow.className = "appWindow";
+            appWindow.style.zIndex = zIndexCounter++; // Make sure it on top
+            document.getElementById("watermark").style.zIndex = zIndexCounter++; // Make sure the watermark always stays on top
+            if (center == true) {
+                appWindow.style.left = `${window.innerWidth/2 - width/2}px`;
+                appWindow.style.top = `${window.innerHeight/2 - height/2}px`;
+            } else {
+                let posX = x/100 * window.innerWidth
+                let posY = y/100 * window.innerHeight
 
-        appWindow.style.left = `${posX}px`;
-        appWindow.style.top = `${posY}px`;
-    }
-    // Check if the user is on mobile to set proper window position
-    if (window.matchMedia("(max-width: 768px)").matches) {
-        appWindow.style.left = `0px`;
-        appWindow.style.top = `0px`;
-    }
-    appWindow.style.width = `${width}px`
-    appWindow.style.height = `${height}px`
-    
-    // Bar
-    const bar = document.createElement("div");
-    bar.className = "bar";
-    appWindow.appendChild(bar);
+                if (posX > (window.innerWidth / 2)) {
+                    posX = posX - width
+                }
+                if (posY > (window.innerHeight / 2)) {
+                    posY = posY - height
+                }
 
-    // App icon
-    const appIcon = document.createElement("div");
-    appIcon.className = "appIcon";
-    appIcon.style.backgroundImage = `url("apps/${app}/icon.png")`;
-    bar.appendChild(appIcon);
-    
-    // App title
-    const appTitle = document.createElement("h3");
-    appTitle.className = "appTitle";
-    appTitle.textContent = title;
-    bar.appendChild(appTitle);
-    
-    // Close button
-    const close = document.createElement("div");
-    close.className = "close";
-    close.addEventListener("click", (e) => {
-        appWindow.classList.add("close-anim");
-        appWindow.addEventListener("animationend", () => {
-            e.stopPropagation();
-            appWindow.remove();
+                appWindow.style.left = `${posX}px`;
+                appWindow.style.top = `${posY}px`;
+            }
+            // Check if the user is on mobile to set proper window position
+            if (window.matchMedia("(max-width: 768px)").matches) {
+                appWindow.style.left = `0px`;
+                appWindow.style.top = `0px`;
+            }
+            appWindow.style.width = `${width}px`
+            appWindow.style.height = `${height}px`
+            
+            // Bar
+            const bar = document.createElement("div");
+            bar.className = "bar";
+            appWindow.appendChild(bar);
+
+            // App icon
+            const appIcon = document.createElement("div");
+            appIcon.className = "appIcon";
+            appIcon.style.backgroundImage = `url("apps/${app}/icon.png")`;
+            bar.appendChild(appIcon);
+            
+            // App title
+            const appTitle = document.createElement("h3");
+            appTitle.className = "appTitle";
+            appTitle.textContent = title;
+            bar.appendChild(appTitle);
+            
+            // Close button
+            const close = document.createElement("div");
+            close.className = "close";
+            close.addEventListener("click", (e) => {
+                appWindow.classList.add("close-anim");
+                appWindow.addEventListener("animationend", () => {
+                    e.stopPropagation();
+                    appWindow.remove();
+                })
+            });
+            close.oncontextmenu = close.oncontextmenu = function() {return false;} // Disable right click menu for the close button
+            bar.appendChild(close);
+            
+            // App area
+            const appArea = document.createElement("iframe");
+            appArea.className = "appArea";
+            appArea.src = `apps/${app}/`
+            appWindow.appendChild(appArea);
+
+            // Resize
+            const resize = document.createElement("div");
+            resize.className = "resize";
+            if (allowResize == true) {
+                appWindow.appendChild(resize);
+            }
+
+            document.body.appendChild(appWindow); // Add the window to the body
         })
-    });
-    close.oncontextmenu = close.oncontextmenu = function() {return false;} // Disable right click menu for the close button
-    bar.appendChild(close);
-    
-    // App area
-    const appArea = document.createElement("div");
-    appArea.className = "appArea";
-    fetch(`apps/${app}/app.html`)
-        .then(response => {
-            return response.text();
-        })
-        .then(text => {
-            appArea.innerHTML = text;
-        });
-    appWindow.appendChild(appArea);
-
-    // Resize
-    const resize = document.createElement("div");
-    resize.className = "resize";
-    if (allowResize == true) {
-        appWindow.appendChild(resize);
-    }
-
-    document.body.appendChild(appWindow); // Add the window to the body
+        .catch(error => console.error('Error:', error));
 }
 
 document.addEventListener('mousedown', (e) => {
@@ -100,6 +107,11 @@ document.addEventListener('mousedown', (e) => {
 
         offsetX = e.clientX - currentDrag.offsetLeft;
         offsetY = e.clientY - currentDrag.offsetTop;
+
+        // Making all app areas un-interactable so they don't break moving the windows
+        document.querySelectorAll(".appArea").forEach(element => {
+            element.style.pointerEvents = "none";
+        });
     }
     
     // Once clicked, loops through parent elements until it finds a window to bring it to top.
@@ -122,6 +134,11 @@ document.addEventListener('mousedown', (e) => {
         // This simple math got me dizzy idk why it took me so long
         offsetX = e.clientX - currentResize.offsetWidth - currentResize.offsetLeft + 4;
         offsetY = e.clientY - currentResize.offsetHeight - currentResize.offsetTop + 4;
+
+        // Making all app areas un-interactable so they don't break moving the windows
+        document.querySelectorAll(".appArea").forEach(element => {
+            element.style.pointerEvents = "none";
+        });
     }
 
     if (e.target.classList.contains("grid")) {
@@ -190,6 +207,11 @@ document.addEventListener('mouseup', () => {
         document.getElementById("drag").classList.add("fadeOutAnim")
         desktopDrag = false;
     }
+
+    // Making all app areas interactable again
+    document.querySelectorAll(".appArea").forEach(element => {
+        element.style.pointerEvents = "all";
+    });
 });
 
 document.getElementById("drag").addEventListener("animationend", () => {
@@ -197,4 +219,4 @@ document.getElementById("drag").addEventListener("animationend", () => {
     document.getElementById("drag").style.display = "none"
 })
 
-createWindow('aboutme', 'About Me!', 0, 0, 820, 510, true, true) // Create the initial window
+createWindow("aboutme") // Create the initial window
